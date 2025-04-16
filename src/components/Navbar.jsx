4295c4baaf81eb001952e2bc52649cb3ceb43e6f
LindaTancini <lindatancini95@gmail.com>
2025-04-16 13:40:21 +0200

    feat: add link and imported

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,15 @@
+//IMPORTAZIONI
+import { Link } from "react-router-dom";
+
 //CREO FUNZIONE NAVBAR
 function Navbar() {
-  return <nav>Sono la Navbar del mio Blog!</nav>;
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">Chi siamo</Link>
+      <Link to="/posts">I miei Post</Link>
+    </nav>
+  );
 }
 
 //ESPORTO
