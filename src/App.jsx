64eaced2f540f391a15e873ae1b43d5cc7b2d660
LindaTancini <lindatancini95@gmail.com>
2025-04-16 13:40:35 +0200

    feat: add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 //IMPORTAZIONI
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import About from "./pages/About";
+import PostList from "./pages/PostsList";
 
 function App() {
   return (
@@ -10,6 +13,14 @@ function App() {
       <BrowserRouter>
         <Header />
         <Navbar />
+        <main>
+          <Routes>
+            {/* CONFIGURO LE ROTTE */}
+            <Route path="/" element={<Homepage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/posts" element={<PostList />} />
+          </Routes>
+        </main>
         <Footer />
       </BrowserRouter>
     </>
