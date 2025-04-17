e07bf971c56b7f43a50e8ad9c3d681b2f97cc0b9
LindaTancini <lindatancini95@gmail.com>
2025-04-17 13:32:13 +0200

    feat: add axios, useState and useEffect

--- src/pages/PostsList.jsx
+++ src/pages/PostsList.jsx
@@ -1,6 +1,34 @@
+//IMPORTAZIONI
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
 //CREO FUNZIONE POSTLIST
 function PostList() {
-  return <h3>Lista dei miei post:</h3>;
+  //CREO STATO PER METTERE I POST IN PAGINA
+  const [posts, setPosts] = useState([]);
+  //CREO CHIAMATA AXIOS CON USE EFFECT
+  const endpoint = "https://jsonplaceholder.typicode.com/posts";
+  useEffect(() => {
+    axios
+      .get(endpoint)
+      .then((res) => setPosts(res.data))
+      .catch((err) => console.error("Errore:", err));
+  }, []);
+
+  return (
+    <div>
+      <h3>Lista dei miei post:</h3>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link to={`/posts/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 //ESPORTO
