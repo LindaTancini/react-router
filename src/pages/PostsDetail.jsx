9c21005573b544ca15438fab3a253cde0565043a
LindaTancini <lindatancini95@gmail.com>
2025-04-17 14:11:17 +0200

    feat: create pages posts detail

---
+++ src/pages/PostsDetail.jsx
@@ -0,0 +1,12 @@
+//CREO FUNZIONE POSTSDETAIL
+function PostsDetail() {
+  return (
+    <div>
+      <h3>Titolo del post</h3>
+      <p>Descrizione del post</p>
+    </div>
+  );
+}
+
+//ESPORTO
+export default PostsDetail;
